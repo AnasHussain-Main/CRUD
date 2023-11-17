18e3be126331249f8d689535fd8ae88bcfdb2627
AnasHussain-Main <muhammad.hussain8@ontariotechu.net>
2023-11-17 03:10:24 -0500

    Minor Updates

--- crud_app/server.js
+++ crud_app/server.js
@@ -3,18 +3,32 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const bodyparser = require('body-parser');
+const path = require('path');
 
 const app = express();
 
 dotenv.config({path:'config.env'})
 const PORT = process.env.PORT || 8080;
 
-//log requests//
+//Log requests//
 app.use(morgan('tiny'));
+
+// Parse request to body-parser//
+app.use(bodyparser.urlencoded({extended: true}))
+
+//set view engine//
+app.set("view engine", "ejs")
+//app.set("views", path.resolve(__dirname, "views/ejs"))//
+
+//load assets
+app.use('/css', express.static(path.resolve(__dirname,"assets/css")))
+app.use('/img', express.static(path.resolve(__dirname,"assets/img")))
+app.use('/js', express.static(path.resolve(__dirname,"assets/js")))
 
 
 app.get('/', (req, res) => {
-    res.send("This is a CRUD application");
+    res.render('index');
 });
 
 app.listen(PORT, () => {
